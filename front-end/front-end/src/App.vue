889410a40c87ec69f246ada99940917a182f75f2
Ole-Kristian Wigum <olemofo@gmail.com>
2025-02-20 15:07:19 +0100

    added calculator frontend

--- front-end/front-end/src/App.vue
+++ front-end/front-end/src/App.vue
@@ -1,30 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import calculator from './components/Calculator.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header class="App-header">
+    <div>
+      CALCULATOR APP
+    </div>
+  </header>
+  <calculator/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+.App-header {
+  background-color: #282c34;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
 }
 </style>
---
+++ front-end/front-end/src/components/Calculator.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const buttonStrings = ['7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '*', 'C', '0', '=', '/']
+
+const inputA = ref('2')
+const inputB = ref('4')  
+const operator = ref('+')
+
+const executeA = ref(false)
+const executeB = ref(false)
+
+const output = ref('')
+
+</script>
+
+<template>
+  <div id = "calc">
+    <div id="calc_display">
+      <label>{{ inputA+' ' }} </label>
+      <label> {{ operator }} </label>
+      <label>{{ ' '+inputB }}</label>
+    </div>
+    <div id="calc_buttons">
+      <button v-for="buttonString in buttonStrings" :key="buttonString" @click="buttonClicked(buttonString)">
+        {{ buttonString }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<style>
+#calc_display {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2em;
+  margin: 1em;
+  width: 300px;
+  background-color: lightgray;
+  border-radius: 10px;
+  border: 4px solid #282c34;
+  padding: 10px;
+  color: #282c34;
+}
+#calc {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 1em;
+  color: white;
+}
+#calc_buttons {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+  margin: 1em;
+  button {
+    background-color: #282c34;
+    border: none;
+    border-radius: 10px;
+    padding: 20px;
+    font-size: 1.5em;
+    cursor: pointer;
+
+  }
+  button:hover {
+    background-color: deepskyblue;
+  }
+}
+</style>
